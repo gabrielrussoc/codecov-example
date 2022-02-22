02b220a8aeda8e0653435b27480504ddba5b6d7f
Gabriel Russo <gabriel.russo@databricks.com>
2022-02-22 14:57:40 +0100

    fix for linux

--- upload_cov.sh
+++ upload_cov.sh
@@ -1,7 +1,6 @@
 set -x
 
 for file in $(find bazel-testlogs/ -name "coverage.dat"); do
-    flag=$(echo $file | awk -F '//' '{print $2}')
-    fix_file=$(sed s,//,/,g <<< $file)
-    ./codecov -t $(cat codecov_token) -C $(git rev-parse HEAD) -f $fix_file -F $flag
+    flag=$(echo $file | awk -F 'bazel-testlogs/' '{print $2}')
+    ./codecov -t $(cat codecov_token) -C $(git rev-parse HEAD) -f $file -F $flag
 done
