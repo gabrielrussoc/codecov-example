45a6c5954cd30081fe1616e2ad34c4446740dc98
Gabriel Russo <gabriel.russo@databricks.com>
2022-02-22 12:54:12 +0100

    flag per file

--- upload_cov.sh
+++ upload_cov.sh
@@ -1,2 +1,7 @@
 set -x
-./codecov -t $(cat codecov_token) -C $(git rev-parse HEAD) -s bazel-testlogs/
+
+for file in $(find bazel-testlogs/ -name "coverage.dat"); do
+    flag=$(echo $file | awk -F '//' '{print $2}')
+    fix_file=$(sed s,//,/,g <<< $file)
+    ./codecov -t $(cat codecov_token) -C $(git rev-parse HEAD) -f $fix_file -F $flag
+done
