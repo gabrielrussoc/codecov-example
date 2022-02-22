b6c8c91cf8166f726933359d3b54fe7a8b60571f
Gabriel Russo <gabriel.russo@databricks.com>
2022-02-22 12:23:46 +0100

    Manytests (#2)
    * pr
    
    * 100 tests

--- partial_coverage.sh
+++ partial_coverage.sh
@@ -1,3 +1,3 @@
-echo "Calculating PARTIAL coverage (//lib2/... for projects"
+echo "Calculating PARTIAL coverage (//lib2/...) for project"
 find bazel-testlogs/ -name coverage.dat -exec rm -rf {} +
 bazel coverage  //lib2/... ; find bazel-testlogs/ -name coverage.dat -exec head -n 3 {} +
