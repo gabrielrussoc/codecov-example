8337a5a3a094f601821687933b32b210b573c8b2
Gabriel Russo <gabriel.russo@databricks.com>
2022-07-20 13:19:55 +0200

    untested

--- lib2/lib.cpp
+++ lib2/lib.cpp
@@ -4,6 +4,12 @@
 
 #include "common/common.h"
 
+void newcode() {
+    for(int i = 0; i < 1; i++) {
+        std::cout << "I'm not tested!";
+    }
+}
+
 void lib2::somethingElse() {
   common::h();
 }
