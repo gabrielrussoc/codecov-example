88fbb07f15477179f5908a723413f588df8d18b8
Gabriel Russo <gabriel.russo@databricks.com>
2022-07-25 14:01:09 +0200

    network

--- upload_cov.sh
+++ upload_cov.sh
@@ -1,4 +1,4 @@
 set -x
 
 file="diff.lcov"
-./codecov -t $(cat codecov_token) -C $(git rev-parse HEAD) -f $file -N 4035c26bd89c88097732178553bd7bc91a084d88
+./codecov -t $(cat codecov_token) -C $(git rev-parse HEAD) -f $file -X network
